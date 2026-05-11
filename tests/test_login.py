3b98d400cef166d5282c636edcfb0698d0a32014
Khayyam Khalilov <mrkhayyamm@gmail.com>
2026-05-11 11:39:30 +0300

    Add logging system to automation framework

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,8 +2,9 @@ import pytest
 from pages.login_page import LoginPage
 from utils.data_reader import *
 from utils.constants import *
+from utils.logger import get_logger
+logger = get_logger()
 
-#YENI BRANCH ACACAKSIN KONUSU: Parallel Test Execution (pytest-xdist)
 
 @pytest.mark.regression
 @pytest.mark.login
@@ -17,14 +18,24 @@ from utils.constants import *
 
 
 
+
 def test_invalid_login(driver,test_data):
+    
+        logger.info("Starting invalid login test")
         login=LoginPage(driver)
+        logger.info(f"trying login with username: {test_data['username']}")
         login.login(
                 test_data["username"],
                 test_data["password"]
         )
-        assert test_data["error"] in login.get_error_message()
-        # assert False
+        actual_error=login.get_error_message()
+        excepted_error=test_data["error"]
+
+        if excepted_error not in actual_error:
+                logger.error(f"Excepted error: {excepted_error}, but got {actual_error}")
+        assert excepted_error in actual_error
+        # assert test_data["error"] in login.get_error_message()
+        logger.info("Invalid login test passed")
         
 
 @pytest.mark.parametrize(
